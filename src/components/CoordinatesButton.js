f47d087cfc73844ada55c23fe481ff4f48ca6bbd
Michael Anderson <michanders@msn.com>
2018-03-01 00:05:21 +0000

    automatically backed up by learn

--- src/components/CoordinatesButton.js
+++ src/components/CoordinatesButton.js
@@ -1,1 +1,12 @@
-// Code CoordinatesButton Component Here
+export default class Clicker extends React.Component {
+ 
+  handleClick = (event) => {
+    console.log(event.type); // prints 'click'
+  }
+ 
+  render() {
+    return (
+      <button onClick={this.handleClick}>Click me!</button>
+    );
+  }
+}
